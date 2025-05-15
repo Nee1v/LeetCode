104e291a2a8c7f3a71fe53444a21d9da8e87d0d3
Nee1v <124910740+Nee1v@users.noreply.github.com>
2025-05-15 09:45:01 -0500

    Update MedianOfTwoSortedArrays.java

--- MedianOfTwoSortedArrays.java
+++ MedianOfTwoSortedArrays.java
@@ -6,19 +6,19 @@ class Solution {
         int p2 = 0;
         while(p1 + p2 < index) {
             if(p1 < nums1.length) {
-                if(p2 < nums2.length) { //both p1 and p2 are valid pointers within the corresponding arrays nums1 and nums2
-                    if(nums1[p1] == nums2[p2]) { //both nums1[p1] and nums2[p2] are the same
+                if(p2 < nums2.length) { // both p1 and p2 are valid pointers within the corresponding arrays nums1 and nums2
+                    if(nums1[p1] == nums2[p2]) { // both nums1[p1] and nums2[p2] are the same
                         p1++;
-                    } else if(nums1[p1] < nums2[p2]) { //nums1[p1] is less than nums2[p2]
+                    } else if(nums1[p1] < nums2[p2]) { // nums1[p1] is less than nums2[p2]
                         p1++;
-                    } else { //nums2[p2] is less than nums1[p1]
+                    } else { // nums2[p2] is less than nums1[p1]
                         p2++;
                     }
-                } else { //p1 is a valid pointer within nums1 but p2 is not a valid pointer within nums2
-                    p1 += (index - p2 - p1); //Could just calculate how much too add
+                } else { // p1 is a valid pointer within nums1 but p2 is not a valid pointer within nums2
+                    p1 += (index - p2 - p1); // Could just calculate how much too add
                 }
-            } else { //p2 is a valid pointer within nums2 but p1 is not a valid pointer within nums1
-                p2 += (index - p1 - p2); //Could just calculate how much to add
+            } else { // p2 is a valid pointer within nums2 but p1 is not a valid pointer within nums1
+                p2 += (index - p1 - p2); // Calculate how much to add
             }
         }
 
@@ -31,22 +31,22 @@ class Solution {
 
         for(int i = 0; i < count; i++) {
             if(p1 < nums1.length) {
-                if(p2 < nums2.length) { //both p1 and p2 are valid pointers within the corresponding arrays nums1 and nums2
-                    if(nums1[p1] == nums2[p2]) { //both nums1[p1] and nums2[p2] are the same
+                if(p2 < nums2.length) { // both p1 and p2 are valid pointers within the corresponding arrays nums1 and nums2
+                    if(nums1[p1] == nums2[p2]) { // both nums1[p1] and nums2[p2] are the same
                         sum += nums1[p1];
                         p1++;
-                    } else if(nums1[p1] < nums2[p2]) { //nums1[p1] is less than nums2[p2]
+                    } else if(nums1[p1] < nums2[p2]) { // nums1[p1] is less than nums2[p2]
                         sum += nums1[p1];
                         p1++;
-                    } else { //nums2[p2] is less than nums1[p1]
+                    } else { // nums2[p2] is less than nums1[p1]
                         sum += nums2[p2];
                         p2++;
                     }
-                } else { //p1 is a valid pointer within nums1 but p2 is not a valid pointer within nums2
+                } else { // p1 is a valid pointer within nums1 but p2 is not a valid pointer within nums2
                     sum += nums1[p1];
                     p1++;
                 }
-            } else { //p2 is a valid pointer within nums2 but p1 is not a valid pointer within nums1
+            } else { // p2 is a valid pointer within nums2 but p1 is not a valid pointer within nums1
                 sum += nums2[p2];
                 p2++;
             }
